94250f183b5cd363675868cf850ba7940e53785d
Reinhold Rumberger <rrumberger@web.de>
2012-08-29 19:54:36 +0200

    Work around an exception on network drives where junction points can't be used for anchoring to at least provide a somewhat useful warning rather than "OpenWrap Shell could not start."

--- src/OpenWrap/Repositories/FolderRepository.cs
+++ src/OpenWrap/Repositories/FolderRepository.cs
@@ -266,9 +266,20 @@ namespace OpenWrap.Repositories
             public override bool? Anchor(IDirectory packagesDirectory, IDirectory packageDirectory, string anchorName)
             {
                 var anchoredDirectory = packagesDirectory.GetDirectory(anchorName);
+                bool isHardLink = true;
+                
+                try
+                {
+                    isHardLink = anchoredDirectory.IsHardLink;
+                }
+                catch (IOException)
+                {
+                    // This happens on drives where information about junction points is not available
+                    isHardLink = false;
+                }
 
-                if (anchoredDirectory.Exists && anchoredDirectory.IsHardLink && !anchoredDirectory.SafeDelete())
-                    return false;
+                if (anchoredDirectory.Exists && isHardLink && !anchoredDirectory.SafeDelete())
+                        return false;
                 anchoredDirectory = packagesDirectory.GetDirectory(anchorName);
                 if (anchoredDirectory.Exists)
                 {
